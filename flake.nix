4a61f1a174a433d44fb382bf2b3624765ee2d55c
dvalinn <tiago.andre.amorim@gmail.com>
2024-11-12 16:49:14 +0000

    flake updates
    flake updates

--- flake.nix
+++ flake.nix
@@ -15,16 +15,46 @@
       #pico-sdk-submodules = (pkgs.pico-sdk.override (previous: {
       #  withSubmodules = true;
       #}));
+
+      flash-util = pkgs.writeShellScriptBin "flash" ''
+        # Check if an argument is provided
+          if [ -z "$1" ]; then
+            echo "Usage: $0 <file.elf>"
+            exit 1
+          fi
+
+        # Run the openocd command with the specified file
+        openocd \
+        -f interface/cmsis-dap.cfg \
+        -f target/rp2040.cfg \
+        -c "adapter speed 5000" \
+        -c "program $1 verify reset exit"
+      '';
+
+      serial-monitor-util = pkgs.writeShellScriptBin "monitor" ''
+        minicom -D /dev/ttyACM0 -b 115200
+      '';
+
+      debug-util = pkgs.writeShellScriptBin "dbg-server" ''
+        openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c 'adapter speed 5000'
+      '';
     in {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           cmake
           gcc-arm-embedded
+          gdb
           libusb1
           openocd
+          # pico-sdk is being built locally
           # pico-sdk-submodules
           picotool
           python3
+          minicom
+
+          flash-util
+          serial-monitor-util
+          debug-util
         ];
         #shellHook = ''
         #  export PICO_SDK_PATH="${pico-sdk-submodules}/lib/pico-sdk"

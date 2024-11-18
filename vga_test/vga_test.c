9eb7c1de4b9b961eec4d552648e6443125e296c1
dvalinn <tiago.andre.amorim@gmail.com>
2024-11-18 18:13:25 +0000

    removed multicore features

--- vga_test/vga_test.c
+++ vga_test/vga_test.c
@@ -7,7 +7,6 @@
 #include <stdio.h>
 
 #include <pico.h>
-#include <pico/multicore.h>
 #include <pico/scanvideo.h>
 #include <pico/scanvideo/composable_scanline.h>
 #include <pico/stdlib.h>
@@ -20,60 +19,28 @@
 #define VGA_MODE vga_mode_320x240_60
 extern const struct scanvideo_pio_program video_24mhz_composable;
 
-// to make sure only one core updates the state when the frame number changes
-// todo note we should actually make sure here that the other core isn't still
-// rendering (i.e. all must arrive before either can proceed - a la barrier)
-static struct mutex frame_logic_mutex;
-
 static void frame_update_logic();
-static void render_scanline(struct scanvideo_scanline_buffer *dest, int core);
+static void render_scanline(struct scanvideo_scanline_buffer *dest);
 
-// "Worker thread" for each core
 void render_loop() {
   static uint32_t last_frame_num = 0;
-  int core_num = get_core_num();
-  printf("Rendering on core %d\n", core_num);
+
   while (true) {
     struct scanvideo_scanline_buffer *scanline_buffer =
         scanvideo_begin_scanline_generation(true);
-    mutex_enter_blocking(&frame_logic_mutex);
-    uint32_t frame_num = scanvideo_frame_number(scanline_buffer->scanline_id);
-    // Note that with multiple cores we may have got here not for the first
-    // scanline, however one of the cores will do this logic first before either
-    // does the actual generation
-    if (frame_num != last_frame_num) {
-      last_frame_num = frame_num;
-      frame_update_logic();
-    }
-    mutex_exit(&frame_logic_mutex);
 
-    render_scanline(scanline_buffer, core_num);
+    frame_update_logic();
+    render_scanline(scanline_buffer);
 
     // Release the rendered buffer into the wild
     scanvideo_end_scanline_generation(scanline_buffer);
   }
 }
 
-struct semaphore video_setup_complete;
-
-void core1_func() {
-  sem_acquire_blocking(&video_setup_complete);
-  render_loop();
-}
-
 int vga_main(void) {
-  mutex_init(&frame_logic_mutex);
-  sem_init(&video_setup_complete, 0, 1);
-
-  // Core 1 will wait for us to finish video setup, and then start rendering
-#ifdef DUAL_CORE_RENDER
-  multicore_launch_core1(core1_func);
-#endif
-
   scanvideo_setup(&VGA_MODE);
   scanvideo_timing_enable(true);
 
-  sem_release(&video_setup_complete);
   render_loop();
   return 0;
 }
@@ -96,7 +63,7 @@ int32_t single_color_scanline(uint32_t *buf, size_t buf_length, int width,
   return 3;
 }
 
-void render_scanline(struct scanvideo_scanline_buffer *dest, int core) {
+void render_scanline(struct scanvideo_scanline_buffer *dest) {
   uint32_t *buf = dest->data;
   size_t buf_length = dest->data_max;
 

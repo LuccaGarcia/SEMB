5ad504a0a4405c8f4222c969d9332df584ba2743
dvalinn <tiago.andre.amorim@gmail.com>
2025-01-07 02:34:56 +0000

    updates to UI

--- project/src/main.c
+++ project/src/main.c
@@ -30,12 +30,10 @@ static void prvLaunchRTOS();
 static struct mutex render_sync_mutex; // Probably unnecessary
 static struct mutex game_state_mutex;  // Probably unnecessary
 
-void render_loop()
-{
+void render_loop() {
   vga_init();
 
-  while (true)
-  {
+  while (true) {
     // Begin scanline generation
     struct scanvideo_scanline_buffer *scanline_buffer =
         scanvideo_begin_scanline_generation(true);
@@ -55,8 +53,7 @@ void render_loop()
   }
 }
 
-void reset_points(struct game_state *gs)
-{
+void reset_points(struct game_state *gs) {
   static struct pong_rect draw_point = {
       .x = 0,
       .y = 20,
@@ -67,58 +64,69 @@ void reset_points(struct game_state *gs)
 
   uint16_t *canvas = vga_get_canvas();
 
-  draw_point.color = 0;
-
-  for (uint i = 0; i < 5; i++)
-  {
+  for (uint i = 0; i < 5; i++) {
     draw_point.x = gs->draw_point_ai.x + i * 10;
     mutex_enter_blocking(&render_sync_mutex);
-    {
-      vga_draw_rectangle_filled(canvas, &draw_point);
-    }
+    { vga_draw_rectangle_filled(canvas, &draw_point); }
     mutex_exit(&render_sync_mutex);
   }
 
-  for (uint i = 0; i < 5; i++)
-  {
+  for (uint i = 0; i < 5; i++) {
     draw_point.x = gs->draw_point_player.x - i * 10 - gs->draw_point_player.w;
     mutex_enter_blocking(&render_sync_mutex);
-    {
-      vga_draw_rectangle_filled(canvas, &draw_point);
-    }
+    { vga_draw_rectangle_filled(canvas, &draw_point); }
     mutex_exit(&render_sync_mutex);
   }
+
   gs->reset_score = false;
 }
 
-void update_canvas(struct game_state *gs)
-{
+void update_canvas(struct game_state *gs) {
+
+  struct pong_rect player_goal = {
+      .x = 20,
+      .y = 0,
+      .w = 1,
+      .h = CANVAS_HEIGHT,
+      .color = (uint16_t)PICO_SCANVIDEO_PIXEL_FROM_RGB5(0x00, 0x33, 0x00),
+  };
 
   struct pong_rect mid_line = {
-      .x = CANVAS_WIDTH / 2 ,
-      .y = 20,
+      .x = CANVAS_WIDTH / 2,
+      .y = 0,
       .w = 1,
-      .h = CANVAS_HEIGHT - 40,
+      .h = CANVAS_HEIGHT,
       .color = (uint16_t)PICO_SCANVIDEO_PIXEL_FROM_RGB5(0xaa, 0xaa, 0xaa),
   };
-  
+
+  struct pong_rect ai_goal = {
+      .x = CANVAS_WIDTH - 20,
+      .y = 0,
+      .w = 1,
+      .h = CANVAS_HEIGHT,
+      .color = (uint16_t)PICO_SCANVIDEO_PIXEL_FROM_RGB5(0x33, 0x00, 0x00),
+  };
+
   uint16_t *canvas = vga_get_canvas();
   mutex_enter_blocking(&render_sync_mutex);
-  {
-    vga_draw_rectangle_filled(canvas, &mid_line);
-  }
+  { vga_draw_rectangle_filled(canvas, &player_goal); }
+  mutex_exit(&render_sync_mutex);
+
+  mutex_enter_blocking(&render_sync_mutex);
+  { vga_draw_rectangle_filled(canvas, &mid_line); }
+  mutex_exit(&render_sync_mutex);
+
+  mutex_enter_blocking(&render_sync_mutex);
+  { vga_draw_rectangle_filled(canvas, &ai_goal); }
   mutex_exit(&render_sync_mutex);
 
   pong_rect objects[] = {gs->ball, gs->player, gs->ai};
   // Render all objects
-  for (uint i = 0; i < sizeof(objects) / sizeof(objects[0]); i++)
-  {
+  for (uint i = 0; i < sizeof(objects) / sizeof(objects[0]); i++) {
     mutex_enter_blocking(&render_sync_mutex);
     {
       mutex_enter_blocking(&game_state_mutex);
-      {
-        vga_draw_rectangle_filled(canvas, &objects[i]);
-      }
+      { vga_draw_rectangle_filled(canvas, &objects[i]); }
       mutex_exit(&game_state_mutex);
     }
     mutex_exit(&render_sync_mutex);
@@ -128,31 +136,24 @@ void update_canvas(struct game_state *gs)
 
   if (gs->reset_score)
     reset_points(gs);
-  else
-  {
-    for (uint i = 0; i < gs->ai_score; i++)
-    {
+  else {
+    for (uint i = 0; i < gs->ai_score; i++) {
       int prev_pos = gs->draw_point_ai.x;
       gs->draw_point_ai.x += i * 10;
 
       mutex_enter_blocking(&render_sync_mutex);
-      {
-        vga_draw_rectangle_filled(canvas, &gs->draw_point_ai);
-      }
+      { vga_draw_rectangle_filled(canvas, &gs->draw_point_ai); }
       mutex_exit(&render_sync_mutex);
 
       gs->draw_point_ai.x = prev_pos;
     }
 
-    for (uint i = 0; i < gs->player_score; i++)
-    {
+    for (uint i = 0; i < gs->player_score; i++) {
       int prev_pos = gs->draw_point_player.x;
       gs->draw_point_player.x -= i * 10 - gs->draw_point_player.w;
 
       mutex_enter_blocking(&render_sync_mutex);
-      {
-        vga_draw_rectangle_filled(canvas, &gs->draw_point_player);
-      }
+      { vga_draw_rectangle_filled(canvas, &gs->draw_point_player); }
       mutex_exit(&render_sync_mutex);
 
       gs->draw_point_player.x = prev_pos;
@@ -160,28 +161,22 @@ void update_canvas(struct game_state *gs)
   }
 
   mutex_exit(&game_state_mutex);
-
 }
 
-static void prvGameLogicTask(void *pvParameters)
-{
+static void prvGameLogicTask(void *pvParameters) {
   struct game_state *gs = pvParameters;
 
   TickType_t xLastWakeTime = xTaskGetTickCount();
   const TickType_t xFrequency = pdMS_TO_TICKS(33);
 
-  for (;;)
-  {
+  for (;;) {
     int move_direction = 0; // Default: no movement
 
     // Map IR commands to movement
-    if (ir_command == IR_C_UP)
-    {
+    if (ir_command == IR_C_UP) {
       move_direction = -1;  // Up
       ir_command = IR_C_OK; // Reset command
-    }
-    else if (ir_command == IR_C_DOWN)
-    {
+    } else if (ir_command == IR_C_DOWN) {
       move_direction = 1;   // Down
       ir_command = IR_C_OK; // Reset command
     }
@@ -192,8 +187,7 @@ static void prvGameLogicTask(void *pvParameters)
       gs_update_ai(gs);
       gs_update_ball(gs);
 
-      if (gs->player_score == 6 || gs->ai_score == 6)
-      {
+      if (gs->player_score == 6 || gs->ai_score == 6) {
         gs->reset_score = true;
         gs->player_score = 0;
         gs->ai_score = 0;
@@ -205,26 +199,22 @@ static void prvGameLogicTask(void *pvParameters)
   }
 }
 
-static void prvGameDrawCanvasTask(void *pvParameters)
-{
+static void prvGameDrawCanvasTask(void *pvParameters) {
   struct game_state *gs = pvParameters; // Not used by task
 
   TickType_t xLastWakeTime = xTaskGetTickCount();
   const TickType_t xFrequency = pdMS_TO_TICKS(25);
 
-  for (;;)
-  {
+  for (;;) {
     update_canvas(gs);
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
   }
 }
 
 // Function to decode NEC protocol from buffered events
-uint32_t process_ir_buffer()
-{
+uint32_t process_ir_buffer() {
   static uint32_t last_message = 0;
-  if (event_count < 3)
-  {
+  if (event_count < 3) {
     printf("Insufficient events to decode\n");
     event_count = 0;
     return 0;
@@ -240,24 +230,21 @@ uint32_t process_ir_buffer()
       !(event_buffer[1].event_kind & GPIO_IRQ_EDGE_RISE) ||
       !(event_buffer[2].event_kind & GPIO_IRQ_EDGE_FALL) ||
       !IR_IN_TIMING_WINDOW(start_pulse_duration, IR_START_PULSE,
-                           IR_TIMING_SLACK_US))
-  {
+                           IR_TIMING_SLACK_US)) {
     event_count = 0;
     printf("Invalid start sequence. Message discarded.\n");
     return 0;
   }
 
   if (IR_IN_TIMING_WINDOW(start_space_duration, IR_REPEAT_SPACE,
-                          IR_TIMING_SLACK_US))
-  {
+                          IR_TIMING_SLACK_US)) {
     event_count = 0;
     printf("Repeat message");
     return last_message;
   }
 
   if (!IR_IN_TIMING_WINDOW(start_space_duration, IR_START_SPACE,
-                           IR_TIMING_SLACK_US))
-  {
+                           IR_TIMING_SLACK_US)) {
     event_count = 0;
     printf("Invalid start sequence. Message discarded.\n");
     return 0;
@@ -268,15 +255,12 @@ uint32_t process_ir_buffer()
   uint8_t bit_count = 0;
   size_t last_fall_index = 2;
 
-  for (size_t i = 3; i < event_count; i++)
-  {
-    if (bit_count == IR_MESSAGE_BIT_MAX)
-    {
+  for (size_t i = 3; i < event_count; i++) {
+    if (bit_count == IR_MESSAGE_BIT_MAX) {
       break; // Stop decoding once all bits are read
     }
 
-    if (event_buffer[i].event_kind & GPIO_IRQ_EDGE_RISE)
-    {
+    if (event_buffer[i].event_kind & GPIO_IRQ_EDGE_RISE) {
       continue; // Ignore rising edges
     }
 
@@ -284,17 +268,12 @@ uint32_t process_ir_buffer()
         event_buffer[i].timestamp - event_buffer[last_fall_index].timestamp;
     last_fall_index = i;
 
-    if (IR_IN_TIMING_WINDOW(delta, IR_LOGIC_1_SPACE, IR_TIMING_SLACK_US))
-    {
+    if (IR_IN_TIMING_WINDOW(delta, IR_LOGIC_1_SPACE, IR_TIMING_SLACK_US)) {
       message = (message << 1) | 1; // Append logic 1
-    }
-    else if (IR_IN_TIMING_WINDOW(delta, IR_LOGIC_0_SPACE,
-                                 IR_TIMING_SLACK_US))
-    {
+    } else if (IR_IN_TIMING_WINDOW(delta, IR_LOGIC_0_SPACE,
+                                   IR_TIMING_SLACK_US)) {
       message = (message << 1); // Append logic 0
-    }
-    else
-    {
+    } else {
       event_count = 0;
       printf("Invalid timing (%llu us). Message discarded.\n", delta);
       return 0;
@@ -310,11 +289,9 @@ uint32_t process_ir_buffer()
 }
 
 // GPIO interrupt callback
-void gpio_callback(uint gpio, uint32_t events)
-{
+void gpio_callback(uint gpio, uint32_t events) {
   (void)gpio;
-  if (event_count >= IR_BUFFER_SIZE)
-  {
+  if (event_count >= IR_BUFFER_SIZE) {
     printf("Buffer overflow! Clearing buffer.\n");
     event_count = 0;
     return;
@@ -325,20 +302,16 @@ void gpio_callback(uint gpio, uint32_t events)
 }
 
 // Timer callback to process and decode events after inactivity
-static void vDecodeTimerCallback(TimerHandle_t xTimer)
-{
+static void vDecodeTimerCallback(TimerHandle_t xTimer) {
   (void)xTimer;
 
   uint64_t current_time = time_us_64();
 
-  if (event_count > 0)
-  {
+  if (event_count > 0) {
     uint64_t latest_event_time = event_buffer[event_count - 1].timestamp;
-    if (current_time - latest_event_time > IR_TIMEOUT_MS)
-    {                                             // 10ms inactivity
+    if (current_time - latest_event_time > IR_TIMEOUT_MS) { // 10ms inactivity
       uint32_t raw_message = process_ir_buffer(); // Decode the buffered events
-      if (!raw_message)
-      {
+      if (!raw_message) {
         return;
       }
 
@@ -350,16 +323,14 @@ static void vDecodeTimerCallback(TimerHandle_t xTimer)
       bool cmd_valid = ((command ^ cmd_inv) == 0xFF);
       bool addr_valid = ((addr ^ addr_inv) == 0xFF);
 
-      if (cmd_valid && addr_valid)
-      {
+      if (cmd_valid && addr_valid) {
         ir_command = command;
       }
     }
   }
 }
 
-int main(void)
-{
+int main(void) {
   TimerHandle_t xIrDecodeTimer = NULL;
 
   prvSetupHardware();
@@ -403,8 +374,8 @@ int main(void)
   };
 
   struct pong_rect AI = {
-      .x = 300,
-      .x_old = 300,
+      .x = CANVAS_WIDTH - 25,
+      .x_old = CANVAS_WIDTH - 25,
       .y = 100,
       .y_old = 100,
       .w = 5,
@@ -423,7 +394,7 @@ int main(void)
   };
 
   struct pong_rect draw_point_ai = {
-      .x = CANVAS_WIDTH / 2 + 20,
+      .x = CANVAS_WIDTH / 2 + 15,
       .y = 20,
       .w = 5,
       .h = 10,
@@ -459,8 +430,7 @@ int main(void)
   prvLaunchRTOS();
 }
 
-static void prvSetupHardware(void)
-{
+static void prvSetupHardware(void) {
   /* Want to be able to printf */
   stdio_usb_init();
 
@@ -474,8 +444,7 @@ static void prvSetupHardware(void)
                                      true, &gpio_callback);
 }
 
-static void prvLaunchRTOS()
-{
+static void prvLaunchRTOS() {
   vTaskStartScheduler();
   /* should never reach here */
   panic_unsupported();
